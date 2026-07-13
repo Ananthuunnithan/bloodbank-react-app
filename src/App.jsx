a97394dda1cc6eac6d7368fa6ca3f8c903789206
Ananthu Unnithan <unnithanananthu69@gmail.com>
2026-07-13 10:26:09 +0530

    completed

--- src/App.jsx
+++ src/App.jsx
@@ -5,14 +5,21 @@ import heroImg from './assets/hero.png'
 import './App.css'
 import addDonor from './components/addDonor'
 import viewDonor from './components/viewDonor'
+import navBar from './components/navBar'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-    <addDonor/>
-    <viewDonor/>
+<BrowserRouter>
+<Routes>
+  <Route path='/' element={<addDonor/>}/>
+  <Route path='/view' element={<viewDonor/>}/>
+</Routes>
+</BrowserRouter>
     </>
   )
 }
